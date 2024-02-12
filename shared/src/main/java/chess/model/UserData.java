19523127e5f4cda7652a576e0f583471ed935f8c
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-02-12 15:42:28 -0700

    Adding models

---
+++ shared/src/main/java/chess/model/UserData.java
@@ -0,0 +1,21 @@
+package chess.model;
+
+/**
+ * Represents user data containing username, password, and email.
+ */
+public class UserData {
+    /**
+     * The username of the user.
+     */
+    public String username;
+
+    /**
+     * The password of the user.
+     */
+    public String password;
+
+    /**
+     * The email of the user.
+     */
+    public String email;
+}
