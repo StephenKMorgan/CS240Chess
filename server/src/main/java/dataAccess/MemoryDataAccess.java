22b4a26a6509905e8573a3cdd546bcbf0c2c4c4a
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-02-24 23:18:22 -0700

    refactored memdata to match the sequence diagram

--- server/src/main/java/dataAccess/MemoryDataAccess.java
+++ server/src/main/java/dataAccess/MemoryDataAccess.java
@@ -1,8 +1,8 @@
 package dataAccess;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.Collection;
 
 import chess.ChessGame;
 import exception.ResponseException;
@@ -12,144 +12,178 @@ import model.UserData;
 
 public class MemoryDataAccess implements DataAccess{
 
-    private Collection<UserData> users;
-    //private HashMap<Integer, UserData> users = new HashMap<>();
-    private Collection<GameData> games;
-    //private HashMap<Integer, GameData> games = new HashMap<>();
-    private Collection<AuthData> authTokens;
-    //private HashMap<Integer, AuthData> authTokens = new HashMap<>();
-
-    // public MemoryDataAccess(HashMap<Integer, UserData> users, HashMap<Integer, GameData> games, HashMap<Integer, AuthData> authTokens) {
-    //     this.users = users;
-    //     this.games = games;
-    //     this.authTokens = authTokens;
-    // }
-
-    public MemoryDataAccess(Collection<UserData> users, Collection<GameData> games, Collection<AuthData> authTokens) {
-        this.users = users;
-        this.games = games;
-        this.authTokens = authTokens;
-    }
-    
+    private HashMap<String, UserData> users = new HashMap<>();
+    private HashMap<Integer, GameData> games = new HashMap<>();
+    private HashMap<String, AuthData> authTokens = new HashMap<>();
 
     public AuthData register(UserData userData) throws ResponseException {
-        //Errors to throw
-        //400 - Bad Request
-        //403 - Already taken
-        //500 - Internal Server Error
-        if (userData == null || userData.username() == null || userData.username().isEmpty() || userData.password() == null || userData.password().isEmpty() ) {
-            throw new ResponseException(400, "Error: Bad Request");
+        //Check to make sure the user is not already taken
+        if (getUser(userData.username()) != null){
+            throw new ResponseException(401, "Error: already taken");
         }
-        if (users == null || users.contains(userData)) {
-            throw new ResponseException(403, "Error: already taken");
-        }
-        users.add(userData);
-        var authToken = new AuthData(UUID.randomUUID().toString(), userData.username());
-        authTokens.add(authToken);
+        //Create the user
+        createUser(userData);
+        //Create a new auth token
+        var authToken = createAuth(userData.username());
         return authToken;
     }
 
     public AuthData login(UserData userData) throws ResponseException {
-        //Errors to throw
-        //400 - Bad Request 
-        //401 - Unauthorized
-        //500 - Internal Server Error
-        if (userData == null || userData.username() == null || userData.username().isEmpty() || userData.password() == null || userData.password().isEmpty() ) {
-            throw new ResponseException(400, "Error: Bad Request");
+        //Get the user from the database
+        if(getUser(userData.username()) == null){
+            throw new ResponseException(500, "Error: Internal Server Error");
         }
-        //Checks the user data against the users in the database and checks if the password is correct
-        if (!users.contains(userData) || users.stream().filter(u -> u.username().equals(userData.username())).findFirst().get().password() != userData.password()) {
+        //Check if the user is valid
+        if (!validateUser(userData)){
             throw new ResponseException(401, "Error: Unauthorized");
         }
-        var authToken = new AuthData(UUID.randomUUID().toString(), userData.username());
-        authTokens.add(authToken);
+        //Create a new auth token
+        var authToken = createAuth(userData.username());
         return authToken;
     }
 
     public void logout(String authToken) throws ResponseException {
-        //Errors to throw
-        //401 - Unauthorized
-        //500 - Internal Server Error
-        if (authToken == null || authToken.isEmpty() || !authTokens.contains(authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get())) {
+        //Check if the auth token is valid
+        if (getAuth(authToken) == null){
             throw new ResponseException(401, "Error: Unauthorized");
         }
-        authTokens.remove(authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get());
+        //Remove the auth token
+        removeAuth(authToken);
     }
 
     public Collection<GameData> listGames(String authToken) throws ResponseException {
-        //Errors to throw
-        //401 - Unauthorized
-        //500 - Internal Server Error
-        if (authToken == null || authToken.isEmpty() || !authTokens.contains(authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get())) {
+        //Check if the auth token is valid
+        if (!validateAuth(authToken)){
             throw new ResponseException(401, "Error: Unauthorized");
         }
-        return games;
+        //Return the list of games
+        return getGames();
     }
 
-    @Override
     public GameData createGame(String authToken, String gameName) throws ResponseException {
-        //Errors to throw
-        //400 - Bad Request
-        //401 - Unauthorized
-        //500 - Internal Server Error
-        if (authToken == null || authToken.isEmpty() || !authTokens.contains(authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get())) {
+        //Check if the auth token is valid
+        if (!validateAuth(authToken)){
             throw new ResponseException(401, "Error: Unauthorized");
         }
-        if (gameName == null || gameName.isEmpty()) {
-            throw new ResponseException(400, "Error: Bad Request");
+        //Create a new game
+        return generateGame(authToken, gameName);
+    }
+
+    public void joinGame(String clientColor, int gameID, String authToken) throws ResponseException {
+        //Check if the auth token is valid
+        if (!validateAuth(authToken)){
+            throw new ResponseException(401, "Error: Unauthorized");
         }
+        //Check if the game is valid
+        if (!validateGame(clientColor, gameID, authToken)){
+            throw new ResponseException(403, "Error: Already taken");
+        }
+        //Join the game
+        joinValidGame(clientColor, gameID, authToken);
+    }
+
+    public void clear() throws ResponseException {
+        users.clear();
+        games.clear();
+        authTokens.clear();
+    }
+
+    //helper functions
+    private UserData getUser(String username) {
+        if (users.containsKey(username)) {
+            return users.get(username);
+        }
+        return null;
+    }
+
+    private void createUser(UserData userData) {
+        users.put(userData.username(), userData);
+    }
+
+    private Boolean validateUser(UserData userData) {
+        var user = getUser(userData.username());
+        if (user == null || !user.password().equals(userData.password())) {
+            return false;
+        }
+        return true;
+    }
+
+    private AuthData createAuth(String username) {
+        var authToken = new AuthData(UUID.randomUUID().toString(), username);
+        authTokens.put(authToken.authToken(), authToken);
+        return authToken;
+    }
+
+    private AuthData getAuth(String authToken) {
+        if (authTokens.containsKey(authToken)) {
+            return authTokens.get(authToken);
+        }
+        return null;
+    }
+
+    private void removeAuth(String authToken) {
+        authTokens.remove(authToken);
+    }
+
+    private boolean validateAuth(String authToken) {
+        if (authTokens.containsKey(authToken)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Collection<GameData> getGames() {
+        return games.values();
+    }
+
+    private GameData generateGame(String authToken, String gameName) {
         int gameID = games.size();
-        var whiteUsername = authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get().username();
+        var whiteUsername = authTokens.get(authToken).username();
         var game = new GameData(gameID, whiteUsername, null, gameName, new ChessGame());
-        games.add(game);
+        games.put(gameID, game);
         return game;
     }
 
-    @Override
-    public void joinGame(String clientColor, int gameID, String authToken) throws ResponseException {
-        // Errors to throw
-        // 400 - Bad Request
-        // 401 - Unauthorized
-        // 403 - Already taken
-        // 500 - Internal Server Error
-        if (authToken == null || authToken.isEmpty() || !authTokens.contains(authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get())) {
-            throw new ResponseException(401, "Error: Unauthorized");
-        }
-        if (clientColor == null || clientColor.isEmpty() || gameID < 0) {
-            throw new ResponseException(400, "Error: Bad Request");
-        }
-        var game = games.stream().filter(g -> g.gameID() == gameID).findFirst().get();
-        if (clientColor.equals("white")) {
-            if (game.whiteUsername() != null) {
-                throw new ResponseException(403, "Error: Already taken");
+    private boolean validateGame(String clientColor, int gameID, String authToken) {
+        //The game is valid if it exists and is not full or if it is full and the user is already in the game or if the user is not in the game but the clientColor is null
+        if (games.containsKey(gameID)) {
+            var game = games.get(gameID);
+            if (clientColor == null) {
+                return true;
             }
-            game = new GameData(game.gameID(), game.whiteUsername(), authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get().username(), game.gameName(), game.game());
-            games.remove(game);
-            games.add(game);
-        } else if (clientColor.equals("black")) {
-            if (game.blackUsername() != null) {
-                throw new ResponseException(403, "Error: Already taken");
+            if (clientColor.equals("white")) {
+                if (game.whiteUsername() == null) {
+                    return true;
+                }
+            } else {
+                if (game.blackUsername() == null) {
+                    return true;
+                }
             }
-            game = new GameData(game.gameID(), authTokens.stream().filter(a -> a.authToken().equals(authToken)).findFirst().get().username(), game.blackUsername(), game.gameName(), game.game());
-            games.remove(game);
-            games.add(game);
+            if (game.whiteUsername() == null || game.blackUsername() == null) {
+                return true;
+            }
+            if (game.whiteUsername().equals(authTokens.get(authToken).username()) || game.blackUsername().equals(authTokens.get(authToken).username())) {
+                return true;
+            }
+            
         }
-
+        return false;
     }
 
-    @Override
-    public void clear() throws ResponseException {
-        // Errors to throw
-        // 500 - Internal Server Error
-        if (users != null) {
-            users.clear();
-        }
-        if (games != null) {
-            games.clear();
-        }
-        if (authTokens != null) {
-            authTokens.clear();
+    private void joinValidGame(String clientColor, int gameID, String authToken) {
+        var game = games.get(gameID);
+        if (clientColor == null) {
+            game = new GameData(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+            games.put(gameID, game);
+        } else {
+            if (clientColor.equals("white")) {
+                game = new GameData(game.gameID(), authTokens.get(authToken).username(), game.blackUsername(), game.gameName(), game.game());
+                games.put(gameID, game);
+            } else {
+                game = new GameData(game.gameID(), game.whiteUsername(), authTokens.get(authToken).username(), game.gameName(), game.game());
+                games.put(gameID, game);
+            }
         }
     }
-    
 }
