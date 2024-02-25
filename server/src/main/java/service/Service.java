e9b77f3e51d83e1144a720af3b4cb1c6867258d3
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-02-24 23:18:43 -0700

    Refactor Service class and add input validation

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -11,41 +11,54 @@ import model.UserData;
 
 
 public class Service {
-    private final DataAccess dataAccess;
+    private final DataAccess dataAccess = new MemoryDataAccess();
 
-
-    public Service(DataAccess dataAccess) {
-        this.dataAccess = dataAccess;
-    }
-
-    public Service() {
-        this.dataAccess = new MemoryDataAccess( null, null, null);
-    }
-
+    //User
     public AuthData register(UserData userData) throws ResponseException {
+        if (userData == null || userData.username() == null || userData.username().isEmpty() || userData.password() == null || userData.password().isEmpty() ) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         return dataAccess.register(userData);
     }
 
+    //Session
     public AuthData login(UserData userData) throws ResponseException {
+        if (userData == null || userData.username() == null || userData.username().isEmpty() || userData.password() == null || userData.password().isEmpty() ) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         return dataAccess.login(userData);
     }
 
     public void logout(String authToken) throws ResponseException {
+        if (authToken == null || authToken.isEmpty()) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         dataAccess.logout(authToken);
     }
 
+    //game
     public Collection<GameData> listGames(String authToken) throws ResponseException {
+        if (authToken == null || authToken.isEmpty()) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         return dataAccess.listGames(authToken);
     }
 
     public GameData createGame(String authToken, String gameName) throws ResponseException {
+        if (authToken == null || authToken.isEmpty() || gameName == null || gameName.isEmpty()) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         return dataAccess.createGame(authToken, gameName);
     }
 
     public void joinGame(String clientColor, int gameID, String authToken) throws ResponseException {
+        if ( gameID < 0 || authToken == null || authToken.isEmpty()) {
+            throw new ResponseException(400, "Error: Bad Request");
+        }
         dataAccess.joinGame(clientColor, gameID, authToken);
     }
 
+    //db
     public void clearAll() throws ResponseException {
         dataAccess.clear();
     }
