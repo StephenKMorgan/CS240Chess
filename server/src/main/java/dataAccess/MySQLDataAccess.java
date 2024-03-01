083648851bf64fb4904dab934ba7c600f9282f4c
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-03-01 14:48:52 -0700

    adding helper functions to match the memory data access pattern

--- server/src/main/java/dataAccess/MySQLDataAccess.java
+++ server/src/main/java/dataAccess/MySQLDataAccess.java
@@ -4,6 +4,9 @@ import java.sql.*;
 import java.util.HashSet;
 import java.util.UUID;
 
+import com.google.gson.Gson;
+
+import chess.ChessGame;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
@@ -81,6 +84,45 @@ public class MySQLDataAccess implements DataAccess {
         throw new UnsupportedOperationException("Unimplemented method 'clear'");
     }
 
+    
+    
+    //Helper functions
+    private UserData getUser(String username) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT * FROM userdata WHERE username = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private void createUser(UserData userData) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "INSERT INTO userdata (username, password, email) VALUES (?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, userData.username());
+            stmt.setString(2, userData.password());
+            stmt.setString(3, userData.email());
+            stmt.executeUpdate();
+        }
+    }
+
+    private Boolean validateUser(UserData userData) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT * FROM userdata WHERE username = ? AND password = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, userData.username());
+            stmt.setString(2, userData.password());
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
+    }
+
     private AuthData createAuth(String username) throws SQLException, DataAccessException {
         try (Connection conn = DatabaseManager.getConnection()) {
             String sql = "INSERT INTO authdata (authID, username, timestamp) VALUES (?, ?, ?)";
@@ -93,7 +135,59 @@ public class MySQLDataAccess implements DataAccess {
             return new AuthData(authToken, username);
         }
     }
+    
+    private AuthData getAuth(String authToken) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT * FROM authdata WHERE authID = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, authToken);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new AuthData(rs.getString("authID"), rs.getString("username"));
+            } else {
+                return null;
+            }
+        }
+    }
 
+    private void removeAuth(String authToken) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "DELETE FROM authdata WHERE authID = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, authToken);
+            stmt.executeUpdate();
+        }
+    }
+
+    private boolean validateAuth(String authToken) throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT * FROM authdata WHERE authID = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, authToken);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
+    }
+
+    private ChessGame convertJsonToChessGame(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, ChessGame.class);
+    }
+
+    private HashSet<GameData> getGames() throws SQLException, DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT * FROM gamedata";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            HashSet<GameData> games = new HashSet<>();
+            while (rs.next()) {
+                String gameJson = rs.getString("game");
+                ChessGame game = convertJsonToChessGame(gameJson);
+                games.add(new GameData(rs.getInt("game_id"), rs.getString("whiteUsername"), rs.getString("blackUsername"), rs.getString("gameName"), game));
+            }
+            return games;
+        }
+    }
     
     private void configureDatabase() throws ResponseException, DataAccessException {
         DatabaseManager.createDatabase();
